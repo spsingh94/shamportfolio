3aa0b68c5e61377ba1bc21f041d9c10bc7ea53be
Shaminder Singh <spsingh94.ss@gmail.com>
2020-08-21 16:58:01 -0400

    updated input size on contact page

--- client/src/components/ContactForm/index.js
+++ client/src/components/ContactForm/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import axios from 'axios';
+import "./style.css";
 
 class ContactForm extends React.Component{
   
@@ -43,15 +44,15 @@ class ContactForm extends React.Component{
   	<form id="contact-form" onSubmit={this.handleSubmit.bind(this)} method="POST">
   	<div className="form-group">
       	<label htmlFor="name">Name</label>
-      	<input type="text" className="form-control" id="name" value={this.state.name} onChange={this.onNameChange.bind(this)} />
+      	<input placeholder="John Doe" type="text" className="form-control" id="name" value={this.state.name} onChange={this.onNameChange.bind(this)} />
   	</div>
   	<div className="form-group">
       	<label htmlFor="exampleInputEmail1">Email address</label>
-      	<input type="email" className="form-control" id="email" aria-describedby="emailHelp" value={this.state.email} onChange={this.onEmailChange.bind(this)} />
+      	<input placeholder="example@example.com" type="email" className="form-control" id="email" aria-describedby="emailHelp" value={this.state.email} onChange={this.onEmailChange.bind(this)} />
   	</div>
   	<div className="form-group">
       	<label htmlFor="message">Message</label>
-      	<textarea className="form-control" rows="5" id="message" value={this.state.message} onChange={this.onMessageChange.bind(this)} />
+      	<textarea placeholder="Your Message" className="form-control" rows="5" id="message" value={this.state.message} onChange={this.onMessageChange.bind(this)} />
   	</div>
   	<button type="submit" className="btn btn-primary">Submit</button>
   	</form>
