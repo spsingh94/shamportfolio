fd2baf420673fb1a6a3dce817f505e4dccbf7e13
Shaminder Singh <spsingh94.ss@gmail.com>
2020-08-21 16:40:24 -0400

    linked credentials from db to mailer in server

--- server.js
+++ server.js
@@ -7,49 +7,25 @@ const fetch = require("node-fetch");
 const nodemailer = require("nodemailer");
 const PORT = process.env.NODE_ENV || 8080;
 
-let credentials;
-var credentialArr = [];
+let getCreds = async function () {
+  let response = await fetch("http://localhost:8080/api/credential");
+  let data = await response.json();
+  return data;
+};
 
-async function getCreds(url = "", data = {}) {
-  const response = await fetch(url, {
-    method: "GET",
-    mode: "cors",
-    cache: "no-cache",
-    credentials: "include",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    redirect: "follow",
-    referrerPolicy: "no-referrer",
-  });
-  return response.json();
-}
+getCreds().then((results) => mailRunner(results));
 
-getCreds("http://localhost:8080/api/credential", { answer: 42 }).then(
-  (data) => {
-    credentials = data;
-    initSeperate();
-  }
-);
-
-
-function initSeperate() {
-  credentials.forEach((credential) => {
-    credentialArr.push(credential.cred);
-  });
-  let user = credentialArr[0];
-  let pass = credentialArr[1];
-
-  const transport = {
-    host: "smtp.gmail.com",
+function mailRunner(creds) {
+  var transport = {
+    host: "smtp.gmail.com", // Don’t forget to replace with the SMTP host of your provider
     port: 587,
     auth: {
-      user: user,
-      pass: pass,
+      user: creds[0].cred,
+      pass: creds[1].cred,
     },
   };
 
-  const transporter = nodemailer.createTransport(transport);
+  var transporter = nodemailer.createTransport(transport);
 
   transporter.verify((error, success) => {
     if (error) {
@@ -60,14 +36,14 @@ function initSeperate() {
   });
 
   router.post("/send", (req, res, next) => {
-    const name = req.body.name;
-    const email = req.body.email;
-    const message = req.body.message;
-    const content = `name: ${name} \n email: ${email} \n message: ${message} `;
+    var name = req.body.name;
+    var email = req.body.email;
+    var message = req.body.message;
+    var content = `name: ${name} \n email: ${email} \n message: ${message} `;
 
     var mail = {
       from: name,
-      to: user,
+      to: creds[0].cred, // Change to email address that you want to receive messages on
       subject: "New Message from Contact Form",
       text: content,
     };
@@ -99,13 +75,13 @@ mongoose.connect(
     useUnifiedTopology: true,
   }
 );
-// getCreds();
 
 // routes
 app.use(require("./routes/api"));
 app.use(cors());
 app.use(express.json());
 app.use("/", router);
+app.listen(3002);
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
