08b0eee29ab74386d51d02c26def334c65052a3d
Shaminder Singh <spsingh94.ss@gmail.com>
2022-06-12 12:38:17 -0500

    update styling for cursor

--- client/src/components/Hero/index.js
+++ client/src/components/Hero/index.js
@@ -1,28 +1,36 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import "./style.css";
 
 const useMove = () => {
-  const [state, setState] = useState({x: 0, y: 0})
+  const [state, setState]     = useState({x: 0, y: 0});
+  const [display, setDisplay] = useState("none");
+
+  useEffect(() => {
+    const timer = setTimeout(() => setDisplay("none"), 500);
+    return () => clearTimeout(timer);
+  })  
 
   const handleMouseMove = e => {
     e.persist()
     setState(state => ({...state, x: e.clientX, y: e.clientY}))
+    setDisplay("initial");
   }
   return {
     x: state.x,
     y: state.y,
+    cursor: display,
     handleMouseMove,
   }
 }
 
 function Hero(props) {
-  const {x, y, handleMouseMove} = useMove();
+  const {x, y, cursor, handleMouseMove} = useMove();
     return (
       <div className="hero parallax" onMouseMove={handleMouseMove} style={{ backgroundImage: `url(${props.backgroundImage})`, height:props.height, backgroundSize:props.backgroundSize}} {...props}>
         <div className="hero-content-container">
           {props.children}
         </div>
-        <div className="animated-cursor" style={{left:`${x}px`, top:`${y}px`}}></div>
+        <div className="animated-cursor" style={{display: cursor, left:`${x}px`, top:`${y}px`}}></div>
       </div>
     );
   }
